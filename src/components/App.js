513268355004edd42cbee34e5e8f12623035e96b
hshockney <shockney@atlanticbb.net>
2019-06-24 09:39:17 -0400

    Added video detail component

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/Youtube';
 import VideoList from './VideoList';
-
+import VideoDetail from './VideoDetail';
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
     onTermSubmit = async (term) => {
@@ -16,12 +16,14 @@ class App extends React.Component {
     };
 
     onVideoSelect = (video) => {
-        
+        this.setState({ selectedVideo: video })  ;  
     };
 
     render() {
         return (
-            <div className="ui container"><SearchBar onFormSubmit={this.onTermSubmit} />
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
             );
