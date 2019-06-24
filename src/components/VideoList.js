0e4de4566efb1e56c15d7083667b6dcd3523b4c7
hshockney <shockney@atlanticbb.net>
2019-06-24 11:06:23 -0400

    Added video player and styled list of videos to be beside player, added default video state

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import VideoItem from './VideoItem';
 const VideoList = ({ videos, onVideoSelect }) => {
     const renderedList = videos.map(video => {
-        return <VideoItem  onVideoSelect={onVideoSelect} video={video} /> ;   
+        return <VideoItem  key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} /> ;   
     });
     return (
         <div className="ui relaxed divided list">
